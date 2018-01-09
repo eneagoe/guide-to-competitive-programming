f41a6ad1a8994f0e74e7d141a521ffa45ca3beaa
eugen neagoe <eneagoe@gmail.com>
2018-01-09 23:01:56 +0200

    Add init script and warning options to build script

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ $# -gt 0 ]
 then
-  g++ -pipe -O2 -std=c++11 -o solveit $1
+  g++ -pipe -O2 -std=c++11 -Wall -o solveit $1
 fi
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cat > $1.cpp <<EOF
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  // TODO
+}
+EOF
