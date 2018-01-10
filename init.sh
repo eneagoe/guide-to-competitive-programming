0191ed59e11a6ed14addae64e2887843dda162e9
eugen neagoe <eneagoe@gmail.com>
2018-01-10 23:18:34 +0200

    Add some shortcut defines and typedefs

--- init.sh
+++ init.sh
@@ -3,7 +3,16 @@
 cat > $1.cpp <<EOF
 #include <bits/stdc++.h>
 
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
 using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
 
 int main() {
   // TODO
